Fixed some bugs and wrong axis labels

analyze_results_main.py
@@ -158,8 +158,8 @@ def ROC_HRD():
         sns.lineplot(fpr, tpr, ci=None)
 
     plt.legend(models, loc='lower right')
-    plt.xlabel('FN')
-    plt.ylabel('TP')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
     plt.savefig(os.path.join(ROOT_DIR, 'results', 'HRD', 'ROC_HRD.pdf'))
     # plt.show()
     np.savetxt(os.path.join(ROOT_DIR, 'results', 'HRD', 'ROC_AUC.tsv'), scores, '%s', '\t')
@@ -229,8 +229,8 @@ def ROC_immunotherapy():
         sns.lineplot(fpr, tpr, ci=None)
 
     plt.legend(models, loc='lower right')
-    plt.xlabel('FN')
-    plt.ylabel('TP')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
     plt.savefig(os.path.join(ROOT_DIR, 'results', 'immunotherapy', 'ROC_immunotherapy.pdf'))
     # plt.show()
     np.savetxt(os.path.join(ROOT_DIR, 'results', 'immunotherapy', 'ROC_AUC.tsv'), scores, '%s', '\t')

src/analyze_results.py
@@ -758,15 +758,15 @@ def compare_panel_clusters():
         corrs.extend(dists_in_clusters)
         corrs.extend(dists_out_clusters)
         models.extend([model] * len(dists_out_clusters) * 2)
-        relations.extend(['intra-cluster pairs'] * len(dists_out_clusters))
-        relations.extend(['inter-cluster pairs'] * len(dists_out_clusters))
+        relations.extend(['Intra-cluster pairs'] * len(dists_out_clusters))
+        relations.extend(['Inter-cluster pairs'] * len(dists_out_clusters))
 
         print(model, len(np.unique(clusters)))
         print(ranksums(dists_in_clusters, dists_out_clusters), np.mean(dists_in_clusters), np.mean(dists_out_clusters))
 
-    df = {'cosine similarity': corrs, 'model': models, 'relation': relations}
+    df = {'Cosine similarity': corrs, 'model': models, 'relation': relations}
     df = pd.DataFrame(df)
-    sns.violinplot(x='relation', y='cosine similarity', hue='model', data=df, split=True, inner='stick')
+    sns.violinplot(x='relation', y='Cosine similarity', hue='model', data=df, split=True, inner='stick')
 
     plt.xlabel('')
     plt.savefig(os.path.join(ROOT_DIR, 'results', 'clusters_quality', 'clusters_quality.pdf'))

src/models/Mix.py
@@ -301,7 +301,7 @@ class Mix:
         expected_pi_sample_cluster, expected_e_sample_cluster, likelihood_sample_cluster = self.pre_expectation_step()
         likelihood_sample_cluster += self.w[:, np.newaxis]
         likelihood_sample_cluster -= logsumexp(likelihood_sample_cluster, 0, keepdims=True)
-        likelihood_sample_cluster = np.exp(likelihood_sample_cluster)
+        likelihood_sample_cluster = np.exp(likelihood_sample_cluster.T)
         self.pi = np.exp(self.pi)
         self.e = np.exp(self.e)
         self.w = np.exp(self.w)
@@ -310,10 +310,7 @@ class Mix:
 
     def weighted_exposures(self, data):
         likelihood_sample_cluster = self.soft_cluster(data)
-        exposures = np.zeros((len(data), self.num_topics))
-        for i in range(self.num_clusters):
-            for j in range(len(data)):
-                exposures[j] += likelihood_sample_cluster[i, j] * self.pi[i]
+        exposures = np.dot(likelihood_sample_cluster, self.pi)
         return exposures
 
     def get_params(self):